052052deac23d96b11633ab6a6983604d4269fa9
Abhishek Bhat <bhatabhishek126@gmail.com>
2025-05-12 20:15:18 +0530

    Second MGOKB Commit

---
+++ api/index.py
@@ -0,0 +1,4 @@
+from run import app
+
+def handler(environ, start_response):
+    return app.wsgi_app(environ, start_response)
--- run.py
+++ run.py
@@ -813,7 +813,7 @@ def delete_option(option_type, option_id):
 if __name__ == '__main__':
     with app.app_context(): 
         add_initial_data() 
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    # app.run(debug=True, host='0.0.0.0', port=5001)
 
 
 
